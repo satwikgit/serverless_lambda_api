59118790d0c501c17317c43f3af69a5e3705eed3
satwik <=>
2018-12-28 22:03:16 +0530

    working cfs

---
+++ handler.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports.list = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
+};
+
+module.exports.create = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
+};
+
+module.exports.read = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
+};
+
+module.exports.update = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
+};
+
+module.exports.delete = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
+};
